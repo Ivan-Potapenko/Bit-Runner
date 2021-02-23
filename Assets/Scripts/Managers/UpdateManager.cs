8ba3a6d49390d1a3a67b5bf91fe7d43d273664b0
1one <van.van123@list.ru>
2021-02-24 05:46:40 +0700

    Update manager

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Managers
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        [SerializeField]
+        private EventDispatcher updateEventDispatcher;
+
+        [SerializeField]
+        private EventDispatcher fixedUpdateEventDispatcher;
+
+        void Update()
+        {   
+            updateEventDispatcher.Dispatch();
+        }
+
+        private void FixedUpdate()
+        {
+            fixedUpdateEventDispatcher.Dispatch();
+        }
+    }
+}
+
