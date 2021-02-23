1e5be645376669b37f1066f0027711b741a4cd95
1one <van.van123@list.ru>
2021-02-24 05:44:42 +0700

    Environment system

---
+++ Assets/Scripts/Gameplay/Environment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environments
+{
+    public class Environment : MonoBehaviour
+    {
+        public GameObject EndOfEvironment;  
+    }
+}
--- Assets/Scripts/Gameplay/EnvironmentGenerator.cs
+++ Assets/Scripts/Gameplay/EnvironmentGenerator.cs
@@ -1,18 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
-public class EnvironmentGenerator : MonoBehaviour
+namespace Environments
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnvironmentGenerator : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private List<GameObject> _environmentPrefabs;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        private EventListener _updateEventListner;
+
+        [SerializeField]
+        private int _objectsCountInPool;
+
+        [SerializeField]
+        private int _distanceToDeactivation = 20;
+
+        private Queue<GameObject> _activeEnvironment;
+
+
+        private List<GameObject> _inactiveEnvironment;
+        private GameObject _endOfLastAddedEvironment;
+
+
+        private void Start()
+        {
+            _inactiveEnvironment = new List<GameObject>();
+            foreach (var prefab in _environmentPrefabs)
+            {
+                for (int i = 0; i < _objectsCountInPool; i++)
+                {
+                    var newEnvironment = Instantiate(prefab);
+                    newEnvironment.SetActive(false);
+                    _inactiveEnvironment.Add(newEnvironment);
+                }
+            }
+            PutEnvironment();
+        }
+
+        private void PutEnvironment()
+        {
+            int indexOfEnvironment = Random.Range(0, _inactiveEnvironment.Count);
+            _endOfLastAddedEvironment = _inactiveEnvironment[indexOfEnvironment].GetComponent<Environment>().EndOfEvironment;
+
+            var environment = _inactiveEnvironment[indexOfEnvironment];
+            _inactiveEnvironment.RemoveAt(indexOfEnvironment);
+
+            environment.SetActive(true);
+            environment.transform.position = new Vector2(0, gameObject.transform.position.y);
+            _activeEnvironment.Enqueue(environment);
+
+
+        }
+
+        private void OnEnable()
+        {
+            _updateEventListner.ActionsToDo += BehaviourUpdate;
+        }
+
+        private void OnDisable()
+        {
+            _updateEventListner.ActionsToDo -= BehaviourUpdate;
+        }
+
+        void DeactivationOfTheEnvironment()
+        {
+            var environment = _activeEnvironment.Dequeue();
+            environment.SetActive(false);
+            _inactiveEnvironment.Add(environment);
+        }
+
+        private void BehaviourUpdate()
+        {
+            if (_endOfLastAddedEvironment.transform.position.y < gameObject.transform.position.y)
+            {
+                PutEnvironment();
+            }
+
+            if ((_activeEnvironment.Peek().transform.position.y + _distanceToDeactivation) < gameObject.transform.position.y)
+            {
+                DeactivationOfTheEnvironment();
+            }
+        }
     }
 }
