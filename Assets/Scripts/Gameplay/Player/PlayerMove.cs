336d53d4fdd0dc059692d189f3d7b724b6ccf885
1one <van.van123@list.ru>
2021-02-26 06:58:02 +0700

    PlayerMove script added

---
+++ Assets/Scripts/Gameplay/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Player
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerSettings _playerSettings;
+
+        [SerializeField]
+        private EventListener _fixedUpdateEventListner;
+
+        [SerializeField]
+        private Rigidbody2D _playerRigidbody;
+
+        private void OnEnable()
+        {
+            _fixedUpdateEventListner.ActionsToDo += BehaviourFixedUpdate;
+        }
+
+        private void OnDisable()
+        {
+            _fixedUpdateEventListner.ActionsToDo -= BehaviourFixedUpdate;
+        }
+
+        private void BehaviourFixedUpdate()
+        {
+            if(_playerSettings.PlayerMove)
+            {
+                _playerRigidbody.MovePosition(transform.position + transform.up * Time.fixedDeltaTime * _playerSettings.PlayerSpeed);
+            }
+        }
+
+    }
+}
+
