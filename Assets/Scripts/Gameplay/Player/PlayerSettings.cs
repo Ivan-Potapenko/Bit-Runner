150c7e3082614d323c4ef52662aae67ca16eeff6
1one <van.van123@list.ru>
2021-02-26 06:58:30 +0700

    PlayerSettings ScriptableObject script added

---
+++ Assets/Scripts/Gameplay/Player/PlayerSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "Player", menuName = "Player Settings")]
+    public class PlayerSettings : ScriptableObject
+    {
+        public float PlayerSpeed = 0;
+        public bool PlayerPositionLeft = false;
+        public bool PlayerMove = true;
+    }
+
+}
