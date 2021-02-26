8daa2bb03a4aa806e4d9dc563e7a3a8ac3c12ccd
1one <van.van123@list.ru>
2021-02-26 07:01:06 +0700

    PlayerControl script added

---
+++ Assets/Scripts/Gameplay/Player/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Player
+{
+    public class PlayerControl : MonoBehaviour
+    {
+        [SerializeField]
+        private EventListener _updateEventListner;
+
+        [SerializeField]
+        private PlayerMove _playerMove;
+
+        private void OnEnable()
+        {
+            _updateEventListner.ActionsToDo += BehaviourUpdate;
+        }
+
+        private void OnDisable()
+        {
+            _updateEventListner.ActionsToDo -= BehaviourUpdate;
+        }
+
+        void BehaviourUpdate()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(Input.mousePosition.x>Screen.width/2)
+                {
+                    print("right");
+                }
+                if (Input.mousePosition.x <= Screen.width / 2)
+                {
+                    print("left ");
+                }
+            }
+        }
+
+    }
+
+}
