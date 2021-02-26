05ff843b5b4f6870bf3026676bc812d2b42b565c
Alex Osi <noemail>
2021-02-27 00:30:51 +0200

    Saving of UI between scenes realized

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [SerializeField]
+    private Button _playButton;
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable() {
+        _playButton.onClick.AddListener(LoadGameplayScene);
+    }
+
+    private void LoadGameplayScene() {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    
+}
